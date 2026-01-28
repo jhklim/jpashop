cca8a80f8b224196ccffe3f5e689447825af241e
jaehyucklim <jhlim087@gmail.com>
2026-01-28 15:44:43 +0900

    feat: JPA 쇼핑몰 도메인 모델링(실전예제4)
    - 주문, 회원, 상품, 카테고리 엔티티 구현
    - JPA 연관관계 매핑(1:N, N:1, 1:1, N:M) 적용
    - 상품(Item) 상속 구조 및 단일 테이블 전략 적용
    - 공통 필드 BaseEntity로 분리

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,8 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.Book;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,13 +15,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = new Order();
-            OrderItem orderItem = new OrderItem();
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("A");
 
-            order.addOrderItem(orderItem);
-
-            em.persist(order);
-            em.persist(orderItem);
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
