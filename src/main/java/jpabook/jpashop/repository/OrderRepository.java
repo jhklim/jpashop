12921af2db7b92e5490d75f8fff22c4d1d7c09a8
jaehyucklim <jhlim087@gmail.com>
2026-02-08 12:45:03 +0900

    refactor: improve order search query using dynamic JPQL

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -22,12 +24,33 @@ public class OrderRepository {
     }
 
     public List<Order> findBySearch(OrderSearch orderSearch) {
-        return em.createQuery("select o from Order o join o.member m " +
-                                " where o.status = :status" +
-                                "   and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000) // 최대 1000건
-                .getResultList();
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+            jpql += " where o.status = :status";
+            isFirstCondition = false;
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query.setParameter("name", "%" + orderSearch.getMemberName() + "%");
+        }
+
+        return query.getResultList();
     }
 }
